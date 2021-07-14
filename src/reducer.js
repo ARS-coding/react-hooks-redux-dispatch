1742ea01e711e31ecdc7b1d046cfbe3cc3fd6b77
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-14 16:27:36 +0300

    Finish code along.

--- src/reducer.js
+++ src/reducer.js
@@ -10,4 +10,15 @@ function changeState(state, action) {
 let state = { count: 0 };
 let action = { type: "counter/increment" };
 
-changeState(state, action);
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+function render() {
+  document.body.textContent = state.count;
+} 
+
+for(let i = 0; i < 3; i++) {
+  console.log(dispatch(action));
+}
